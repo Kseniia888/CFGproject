1dfe484da38a8617ff970ed02587e3b3e3d374ad
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-07-09 19:04:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import requests
+
+
+ingredient = input('What ingredient should be in recipes? ')
+
+def recipe_search(ingredient):
+# Register to get an APP ID and key https://developer.edamam.com/
+    app_id = 'a2a43e33'
+    app_key = '7230c73e5b23cf4676d66b140694077c'
+    result = requests.get(
+        'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,
+        app_key)
+    )
+    data = result.json()
+    return data['hits']
+
+
+def run():
+    results = recipe_search(ingredient)
+
+    for result in results:
+        recipe = result['recipe']
+
+
+        print(recipe['label'])
+        print(recipe['shareAs'])
+        print()
+
+
+run()
