fae332f5710d311ea408496a738ccd53e797aa4c
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-07-13 10:10:04 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-
+import pprint
 import requests
+import json
 
-
-
+ingredient = input('What ingredient should be in recipes? ')
 
 def recipe_search(ingredient):
-# Register to get an APP ID and key https://developer.edamam.com/
+    # Register to get an APP ID and key https://developer.edamam.com/
     app_id = 'a2a43e33'
     app_key = '7230c73e5b23cf4676d66b140694077c'
     result = requests.get(
-        'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,
-        app_key)
+        'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
     )
     data = result.json()
     return data['hits']
 
-
 def run():
-    ingredient = input('What ingredient should be in recipes? ')
     results = recipe_search(ingredient)
 
     for result in results:
@@ -30,3 +27,14 @@ def run():
 
 
 run()
+
+def create_and_write_json_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+    print(f"Data written to '{filename}' file.")
+
+data_from_data = recipe_search(ingredient)
+create_and_write_json_file(data_from_data, 'data.json')
+
+data_from_data = sorted(data_from_data, key=lambda x: x['recipe']['totalWeight'], reverse=True)
+pprint.pprint(data_from_data)
